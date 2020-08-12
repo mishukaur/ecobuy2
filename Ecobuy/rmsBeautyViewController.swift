6bb8dd47f513b965bff6bde536fe04d09c5af1e3
mishukaur <69322074+mishukaur@users.noreply.github.com>
2020-08-11 22:30:58 -0400

    Add files via upload

---
+++ Ecobuy/rmsBeautyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  rmsBeautyViewController.swift
+//  Ecobuy
+//
+//  Created by Manjot Kaur on 8/11/20.
+//  Copyright © 2020 kwk. All rights reserved.
+//
+
+import UIKit
+
+class rmsBeautyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
